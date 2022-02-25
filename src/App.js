424c0e00b5cf7ad16cb1fa817b6591a76328759b
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-02-25 19:14:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { data, statuses } from "./data/index";
+import "./App.css";
+
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+import DropWrapper from "./components/DropWrapper";
+import Col from "./components/Col";
+import Item from "./components/Item";
 
 function App() {
+  const [items, setItems] = useState(data);
+
+  const onDrop = (item, monitor, status) => {
+    const mapping = statuses.find((si) => si.status === status);
+
+    setItems((prevState) => {
+      const newItems = prevState
+        .filter((i) => i.id !== item.id)
+        .concat({ ...item, status, icon: mapping.icon });
+      return [...newItems];
+    });
+  };
+
+  const moveItem = (dragIndex, hoverIndex) => {
+    const item = items[dragIndex];
+    setItems((prevState) => {
+      const newItems = prevState.filter((i, idx) => idx !== dragIndex);
+      newItems.splice(hoverIndex, 0, item);
+      return [...newItems];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className={"row"}>
+        {statuses.map((s) => {
+          return (
+            <div key={s.status} className={"col-wrapper"}>
+              <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
+              <DropWrapper onDrop={onDrop} status={s.status}>
+                <Col>
+                  {items
+                    .filter((i) => i.status === s.status)
+                    .map((i, idx) => (
+                      <Item
+                        key={i.id}
+                        item={i}
+                        index={idx}
+                        moveItem={moveItem}
+                        status={s}
+                      />
+                    ))}
+                </Col>
+              </DropWrapper>
+            </div>
+          );
+        })}
+      </div>
+    </DndProvider>
   );
 }
 
---
+++ src/data/index.js
@@ -0,0 +1,33 @@
+const data = [
+  {
+    id: 1,
+    status: "open",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 2,
+    status: "open",
+    content: "Get an anniversary gift",
+  },
+  {
+    id: 3,
+    status: "open",
+    content: "Call the bank to talk about investments",
+  },
+  {
+    id: 4,
+    status: "open",
+    content: "Finish reading Intro to UI/UX",
+  },
+];
+
+const statuses = [
+  {
+    status: "open",
+  },
+  {
+    status: "in progress",
+  },
+];
+
+export { data, statuses };
